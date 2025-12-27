f0f821ab6adb489f14e52c841e61c4e441bcc6b4
Emad <emadtoranji6@gmail.com>
2025-12-28 00:33:17 +0330

    improve robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,13 +1,37 @@
 import BaseUrlAddress from '@utils/BaseUrlAddress';
 
-export const dynamic = 'force-static';
+export const revalidate = 3600 * 24 * 30; // 30 days
 
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: `${BaseUrlAddress}sitemap.xml`,
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: disallow,
+      },
+      {
+        userAgent: ['Googlebot', 'Bingbot', 'Slurp'],
+        allow: '/',
+        disallow: disallow,
+      },
+      {
+        userAgent: [
+          'GPTBot',
+          'ChatGPT-User',
+          'ClaudeBot',
+          'Claude-Web',
+          'PerplexityBot',
+          'Google-Extended',
+          'Meta-ExternalAgent',
+          'Meta-ExternalFetcher',
+          'CCBot',
+          'Amazonbot',
+        ],
+        allow: '/',
+        disallow: disallow,
+      },
+    ],
+    sitemap: `${BaseUrlAddress}/sitemap.xml`,
   };
 }
