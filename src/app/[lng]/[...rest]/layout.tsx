802ff4c0b0b6969bf6c512a28c2fe5202685a255
Emad Toranji <emadtoranji6@gmail.com>
2026-08-20 20:26:33 +0330

    add generateStaticParams

--- src/app/[lng]/[...rest]/layout.tsx
+++ src/app/[lng]/[...rest]/layout.tsx
@@ -1,11 +1,22 @@
 import type { Metadata } from 'next';
 import type { GenerateMetadataProps } from '@utils/metadata';
 import React from 'react';
+import { languages } from '@i18n/settings';
 
 export const generateMetadata = (props: GenerateMetadataProps): Promise<Metadata> =>
   import('@utils/metadata').then((m) => m.generateMetadata(props, 'not-found', false, false));
 
 export const dynamic = 'force-static';
+
+export async function generateStaticParams() {
+  const routes: { lng: string; rest: string[] }[] = [];
+
+  for (const lng of languages) {
+    routes.push({ lng, rest: ['not-found'] });
+  }
+
+  return routes;
+}
 
 export default function NotFoundLayout({ children }: { children: React.ReactNode }) {
   return children;
