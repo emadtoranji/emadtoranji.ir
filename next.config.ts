a088c2b666503db376d2955b1feeb654eed99fa6
Emad Toranji <emadtoranji6@gmail.com>
2026-08-20 20:23:41 +0330

    add output

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: process.env.NEXT_EXPORT === 'true' ? 'export' : undefined,
   reactCompiler: true,
   trailingSlash: false,
   poweredByHeader: false,
